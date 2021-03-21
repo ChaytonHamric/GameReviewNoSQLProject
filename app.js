53f2a92165535c5cd7a0aa3eafd365999630bef0
Chayton Hamric <ChaytonHamric@gmail.com>
2021-03-20 21:09:15 -0400

    push!

--- app.js
+++ app.js
@@ -13,28 +13,29 @@ const express = require('express');
 const Review = require('./models/Reviews')
 
 const app = express();
+function add_review(){
+    mongoose.connect('mongodb+srv://Admin:Nosql123@gamereviewsite.xhhn6.mongodb.net/Game-Review?retryWrites=true&w=majority',
+                        { useNewUrlParser: true, useUnifiedTopology: true})
+                        .then((result) => app.listen(3000))
+                        .catch((err) => console.log(err));
 
-mongoose.connect('mongodb+srv://Admin:Nosql123@gamereviewsite.xhhn6.mongodb.net/Game-Review?retryWrites=true&w=majority',
-                    { useNewUrlParser: true, useUnifiedTopology: true})
-                    .then((result) => app.listen(3000))
-                    .catch((err) => console.log(err));
-
-app.get('/add-review', (req, res) => {
-    const review = new Review({
-        GameName: "Pokemon",
-        HoursPlayed: 500,
-        FiveStarRating: 5,
-        Review: "Great Game"
-    });
-
-    review.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
+    app.get('/add-review', (req, res) => {
+        const review = new Review({
+            GameName: "Digimon",
+            HoursPlayed: 500,
+            FiveStarRating: 5,
+            Review: "Great Game"
         });
-})
+
+        review.save()
+            .then((result) => {
+                res.send(result)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    })
+}
 
 app.get('/all-reviews', (req, res) => {
     Review.find()
