e6f374c75c818ecdec2563afb7f143384c0afb67
Chayton Hamric <ChaytonHamric@gmail.com>
2021-04-19 16:57:29 -0400

    What we have so far

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var http = require('http');
 var fs = require('fs');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const PORT = 3000;
 
-var xhr = newXMLHttpRequest();
-vhr.open("POST", "http://id.twitch.tv/oath2/token?client_id=g9ld31alrkoaably90beduok0snere&client_secret=6ju82oxji3lt6v5fp9idsc1d18gm4g&grant_type=client_credentials"), true
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify({
-    value: value
-}));
+// var xhr = new XMLHttpRequest();
+// xhr.open("POST", "http://id.twitch.tv/oath2/token?client_id=g9ld31alrkoaably90beduok0snere&client_secret=6ju82oxji3lt6v5fp9idsc1d18gm4g&grant_type=client_credentials", true);
+// xhr.setRequestHeader('Content-Type', 'application/json');
+// xhr.send(null)
+// xhr.send(JSON.stringify({
+//     value: value
+// }));
 fs.readFile('index.html', function (err, html) {
 
     if (err) throw err;
