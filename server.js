e0ab884fe90c2222f34fc1762d278e06bf1bb935
A.Karwowski <akk42@zips.uakron.edu>
2021-04-19 21:20:27 -0400

    Localhost up and working

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express')
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+console.log("WHAT UP")
 
-const PORT = 3000;
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.use(express.static(path.join(__dirname + "/frontend/")))
+app.get('/', function(req, res) {
+    
+    res.sendFile(path.join(__dirname + '/frontend/index.html'));
+});
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var xhr = new XMLHttpRequest();
 // xhr.open("POST", "http://id.twitch.tv/oath2/token?client_id=g9ld31alrkoaably90beduok0snere&client_secret=6ju82oxji3lt6v5fp9idsc1d18gm4g&grant_type=client_credentials", true);
@@ -11,14 +38,3 @@ const PORT = 3000;
 // xhr.send(JSON.stringify({
 //     value: value
 // }));
-fs.readFile('index.html', function (err, html) {
-
-    if (err) throw err;
-
-    http.createServer(function (request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    }).listen(PORT);
-});
-
