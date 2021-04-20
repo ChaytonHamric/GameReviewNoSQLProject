9cca0c6cb1d405241d6b012d247751d9d468816c
A.Karwowski <akk42@zips.uakron.edu>
2021-04-20 02:18:23 -0400

    add review list component

---
+++ frontend/components/ReviewList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Review from './Review'
+export default class ReviewList extends Component {
+    render() {
+        return (
+            <div>
+                {{
+                    this.props.reviews.map(review=><Review stars={review.stars} hours={review.hours} review={review.review} />)
+                }}
+            </div>
+        )
+    }
+
+}
