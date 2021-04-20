c18643025785546d92f41d7f4047aae6ef8311e2
A.Karwowski <akk42@zips.uakron.edu>
2021-04-19 22:13:40 -0400

    add read controller

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,13 +1,29 @@
+const mongoose = require('mongoose');
+const Review = require('../models/Reviews')
+ 
+
 module.exports = {
     create: (req, res) => {
         res.status(201).json({
             message: "Successfully Created"
         })
     },
-    read: (req, res) => {
-        res.status(201).json({
-            message: "Successfully Created"
-        })
+    read: async (req, res) => { 
+        try {
+            const review = await Review.findById(req.params.reviewId);
+            res.status(201).json({
+            review: {
+                ...review._doc,
+                _id: review.id,
+                HoursPlayed: review.HoursPlayed,
+                FiveStarRating: review.FiveStarRating,
+                Review: review.Review
+            }
+            });
+        }
+        catch (err){
+            throw err;
+        }
     },
     readGame: async (req, res) => {
         try{
