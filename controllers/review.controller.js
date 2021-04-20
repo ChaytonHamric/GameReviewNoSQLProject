e444ae41c3359ccd52a2481265f705d756957ba9
A.Karwowski <akk42@zips.uakron.edu>
2021-04-19 22:13:13 -0400

    add read game control

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -9,9 +9,26 @@ module.exports = {
             message: "Successfully Created"
         })
     },
-    readGame: (req, res) => {
-        res.status(201).json({
-            message: "Successfully Created"
-        })
+    readGame: async (req, res) => {
+        try{
+            const reviews = await Review.find({
+                GameName: req.params.GameName
+            });
+            transformed_Reviews = reviews.map(review => {
+                return {
+                    ...review._doc,
+                    _id: review.id,
+                    HoursPlayed: review.HoursPlayed,
+                    FiveStarRating: review.FiveStarRating,
+                    Review: review.Review
+                }
+            });
+            res.status(201).json({
+                reviews: transformed_Reviews
+            });
+        }
+        catch (err){
+            throw err;
+        }
     } 
 }
