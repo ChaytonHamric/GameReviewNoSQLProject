199e3992a5589e59670b3dec4d5f347a995f487f
A.Karwowski <akk42@zips.uakron.edu>
2021-04-20 02:16:34 -0400

    add anchor component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,1 +1,9 @@
 import React, { Component } from 'react'
+
+export default class App extends Component {
+    render(){
+        return (
+            <h1>17 Robots is a god send</h1>
+        )
+    }
+}
