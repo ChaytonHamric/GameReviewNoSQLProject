ca4134144b937e521f5ea3977d74a45989d029fe
A.Karwowski <akk42@zips.uakron.edu>
2021-04-19 21:46:24 -0400

    add controller

---
+++ controllers/review.controller.js
@@ -0,0 +1,17 @@
+module.exports = {
+    create: (req, res) => {
+        res.status(201).json({
+            message: "Successfully Created"
+        })
+    },
+    read: (req, res) => {
+        res.status(201).json({
+            message: "Successfully Created"
+        })
+    },
+    readGame: (req, res) => {
+        res.status(201).json({
+            message: "Successfully Created"
+        })
+    } 
+}
