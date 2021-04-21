cb2ac7ae9c8acc715ce7ec7eff25f18da1204391
A.Karwowski <akk42@zips.uakron.edu>
2021-04-21 17:25:33 -0400

    fix review list

--- frontend/components/ReviewList.js
+++ frontend/components/ReviewList.js
@@ -4,9 +4,9 @@ export default class ReviewList extends Component {
     render() {
         return (
             <div>
-                {{
+                {
                     this.props.reviews.map(review=><Review stars={review.stars} hours={review.hours} review={review.review} />)
-                }}
+                }
             </div>
         )
     }
