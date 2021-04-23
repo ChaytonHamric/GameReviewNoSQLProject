6911dfa33297c4c67f9311032b2ca6fff4c41afa
A.Karwowski <akk42@zips.uakron.edu>
2021-04-23 11:57:38 -0400

    handle submission, update forms, add star list

--- frontend/components/CreateReview.js
+++ frontend/components/CreateReview.js
@@ -1,23 +1,71 @@
 import React, { Component } from "react";
-
+import star from "../star.png"
+import graystar from "../graystar.png"
+import StarList from './StarList'
 export default class CreateReview extends Component {
+    constructor(props) {
+        super(props)
+        this.hoursEl = React.createRef()
+        this.reviewEl = React.createRef();
+        this.state = {
+            starCount: 0
+        }
+    }
+
+    updateStarCount(stars) {
+        this.setState({starCount: stars})
+    }
+
+    clickHandler(e) {
+        const hours = this.hoursEl.current.value
+        const review = this.reviewEl.current.value
+        const stars = this.state.starCount
+        const gameName = this.props.gameName
+
+        fetch("http://localhost:3000/reviews/create/", {
+          headers: {
+            'Accept': "application/json",
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify({
+            GameName: gameName,
+            HoursPlayed: hours,
+            FiveStarRating: stars,
+            Review: review,
+          }),
+        })
+          .then((res) => res.json())
+          .then((body) => {
+              const newReviews = [...this.props.currentReviewList]
+              newReviews.push({
+                stars: body.FiveStarRating,
+                review: body.Review,
+                hours: body.HoursPlayed
+              })
+            this.props.updateReviewList(newReviews)
+          });
+        e.preventDefault()
+    }
+
     render(){
         return(
-            <div>
-                <form>
+            <div style={{margin: '35px'}}>
+                <form id="myform" action="" >
                     <div className="form-group">
-                        <label for="Hours Played">Hours Played</label>
-                        <input required className="form-control" type="number" id="HoursPlayed" name="Hours Played" min="1" max="1000"></input>
+                        <label htmlFor="HoursPlayed">Hours Played</label>
+                        <input ref={this.hoursEl} required className="form-control" type="number" id="HoursPlayed" name="HoursPlayed" min="1" max="1000"></input>
                     </div>
                     <div className="form-group">
-                        <label for="Five Star Rating">Five Star Rating</label>
-                        <input required className="form-control" type="number" id="FiveStarRating" name="Five Star Rating" min="0" max="5"></input>
+                        {/* <label for="FiveStarRating">Five Star Rating</label>
+                        <input required className="form-control" type="number" id="FiveStarRating" name="FiveStarRating" min="0" max="5"></input> */}
+                        <StarList updateFormStars={this.updateStarCount.bind(this)}/>
                     </div>
                     <div className="form-group">
-                        <label for="Review">Bare Bones Review</label>
-                        <input required className="form-control" type="text" id="Review" name="Review" defaultValue="Type your review here" maxLength="120"></input>
+                        <label htmlFor="Review">Bare Bones Review</label>
+                        <input required ref={this.reviewEl} className="form-control" type="text" id="Review" name="Review" defaultValue="Type your review here" maxLength="120"></input>
                     </div>
-                    <input className="btn btn-primary"type="submit"></input>
+                    <button className="btn btn-primary" onClick={this.clickHandler.bind(this)}>Submit</button>
                 </form>
             </div>
             
