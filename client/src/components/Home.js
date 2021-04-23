0207223de505ba79903e6ddcd0a23a76c1105919
A.Karwowski <akk42@zips.uakron.edu>
2021-04-23 18:35:47 -0400

    fix address

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -26,7 +26,7 @@ export default class Home extends Component {
     e.preventDefault();
     this.setState({ isLoading: true });
     const search = this.searchEL.current.value;
-    fetch(`http://localhost:3000/games/`, {
+    fetch(`http://157.230.63.172:3001/games/`, {
       method: "POST",
       headers: {
         Accept: "application/json",
