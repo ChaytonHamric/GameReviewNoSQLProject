f0d0c65376c0efa387d139e1e942011bff17b57b
A.Karwowski <akk42@zips.uakron.edu>
2021-04-22 21:16:55 -0400

    better search and listing

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,50 +1,80 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import Review from './Review'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import Loader from "./Loader";
+
 export default class Home extends Component {
-    
-    constructor(props){
-        super(props);
-        this.searchEL = React.createRef();
+  constructor(props) {
+    super(props);
+    this.searchEL = React.createRef();
 
-        this.state = {
-            gameList: [] 
-        }
-    }
-    loadGameTitles(){
-
-        fetch(`http://localhost:3000/games/`)
-          .then((res) => res.json())
-          .then((titleList) => {
-            let titles = [];
-            titleList.titles.forEach((title) => {
-              titles.push(title);
-            });
-            return titles;
-          })
-          .then((titles) => this.setState({gameList: titles}))
-
+    this.state = {
+      gameList: [],
+      isLoading: false,
+      gameName: "",
     };
-    componentDidMount(){
-        this.loadGameTitles();
-    }
-    render() {
-        return (
-          //class for styling = className="text"
-          <div>
-            <h1>Bare Bones Gaming</h1>
-            <hr />
+    this.updateList = this.updateList.bind(this);
+  }
+
+  componentDidMount() {}
+
+  updateList(e) {
+    e.preventDefault();
+    this.setState({ isLoading: true });
+    const search = this.searchEL.current.value;
+    fetch(`http://localhost:3000/games/`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        GameName: `${search}`,
+      }),
+    })
+      .then((res) => {
+        let response = res.json();
+        console.log(response);
+        return response;
+      })
+      .then((titleList) => {
+        this.setState({ gameList: titleList.titles, isLoading: false });
+      });
+  }
+  render() {
+    return (
+      //class for styling = className="text"
+      <div>
+        <h1>Bare Bones Gaming</h1>
+        <hr />
+        <div>
+          <form onSubmit={this.updateList}>
+            <input type="text" ref={this.searchEL}></input>
+          </form>
+          {this.state.isLoading ? (
             <div>
-              <input type="text" ref={this.searchEL}></input>
-              <ul>
-                {this.state.gameList.map((game) => (
-                  <li>
-                    <Link to={`/title/${encodeURI(game.Name.split(" ").join(""))}`}>{game.Name}</Link>
-                  </li>
-                ))}
-              </ul>
+              <h2>
+                {this.searchEL.current.value == ""
+                  ? "Fetching Random Game"
+                  : `Loading search results for ${this.searchEL.current.value}`}
+              </h2>
+              <Loader />
             </div>
-          </div>
-        );
-    }
+          ) : (
+            <ul>
+              {this.state.gameList.map((game) => (
+                <li>
+                  <Link
+                    to={`/title/${encodeURI(game.Name.split(" ").join(""))}`}
+                  >
+                    {game.Name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
