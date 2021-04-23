aa61f6dcd11cc08efc67c08e8c4cdd7540fbaf97
A.Karwowski <akk42@zips.uakron.edu>
2021-04-22 21:17:52 -0400

    make review list functional

--- frontend/components/ReviewList.js
+++ frontend/components/ReviewList.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'
-import Review from './Review'
+import React, { Component } from "react";
+import Review from "./Review";
 export default class ReviewList extends Component {
-    render() {
-        return (
-            <div>
-                {
-                    this.props.reviews.map(review=><Review stars={review.stars} hours={review.hours} review={review.review} />)
-                }
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        {this.props.reviews.map((review) => (
+          <Review
+            stars={review.stars}
+            hours={review.hours}
+            review={review.review}
+          />
+        ))}
+      </div>
+    );
+  }
 }
