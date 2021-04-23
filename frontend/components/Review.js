17c1fb93b0ebc2d8a16741a46a291ba358059db4
A.Karwowski <akk42@zips.uakron.edu>
2021-04-22 21:17:37 -0400

    clean up reviews

--- frontend/components/Review.js
+++ frontend/components/Review.js
@@ -1,40 +1,32 @@
-import React, { Component } from 'react'
- 
-export default class Review extends Component {
-    
-    componentDidMount() {
-        for(var i = 0; i < this.props.stars; ++i){
-            this.setState(prevState =>({
-                stars: [
-                    ...prevState.stars,
-                    <img className="star-image" src="">
+import React, { Component } from "react";
+import star from "../star.png";
 
-                    </img>
-                ]
-            }));
-        }
+export default class Review extends Component {
+  componentDidMount() {
+    for (var i = 0; i < this.props.stars; ++i) {
+      this.setState((prevState) => ({
+        stars: [
+          ...prevState.stars,
+          <img className="star-image" src={star}></img>,
+        ],
+      }));
     }
-    constructor(props){
-        super(props);
-        this.state = {
-            stars: []
-        };
-    }
-    render() {
-        return(
-        <div>
-            <div>
-                {this.state.stars}
-            </div>
-            <p>
-                {this.props.review}
-            </p>
-            <ul>
-                <li>
-                    {this.props.hours}
-                </li>
-            </ul>
-        </div>
-        )
-    }
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      stars: [],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div>{this.state.stars}</div>
+        <p>{this.props.review}</p>
+        <ul>
+          <li>{this.props.hours}</li>
+        </ul>
+      </div>
+    );
+  }
 }
