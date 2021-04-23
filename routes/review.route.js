6706f8e439a10360ee5d493cd59f4bd7d6e3e1c4
A.Karwowski <akk42@zips.uakron.edu>
2021-04-22 21:18:51 -0400

    a lot of small minor changes like ' -> "

--- routes/review.route.js
+++ routes/review.route.js
@@ -1,12 +1,10 @@
-const express = require('express');
+const express = require("express");
 const urlRoutes = express.Router();
-const bodyParser = require('body-parser')
-
-
+const bodyParser = require("body-parser");
 
 const controller = require("../controllers/review.controller");
-urlRoutes.post("/create", bodyParser.json() ,controller.create);
-urlRoutes.get('/:GameName', controller.readGame);
-urlRoutes.get('/:GameName/:Review_ID', controller.read);
+urlRoutes.post("/create", bodyParser.json(), controller.create);
+urlRoutes.get("/:GameName", controller.readGame);
+urlRoutes.get("/:GameName/:Review_ID", controller.read);
 
 module.exports = urlRoutes;
