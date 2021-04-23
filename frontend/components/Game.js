e8ccddf6ef0cab09431b01b53190136940d60302
A.Karwowski <akk42@zips.uakron.edu>
2021-04-22 21:16:36 -0400

    fetch changes and add Create review

--- frontend/components/Game.js
+++ frontend/components/Game.js
@@ -1,31 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
-import ReviewList from './ReviewList'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import CreateReview from "./CreateReview";
+import ReviewList from "./ReviewList";
 
 const Game = () => {
   const { name } = useParams();
   const [reviewList, setReviewList] = useState([]);
-
-  fetch(`http://localhost:3000/reviews/${name}`).then(res => res.json()).then(list => {
-    let reviews = []
-    list.reviews.forEach(review => {
-        reviews.push({
+  useEffect(() => {
+    fetch(`http://localhost:3000/reviews/${name}`)
+      .then((res) => res.json())
+      .then((list) => {
+        let reviews = [];
+        list.reviews.forEach((review) => {
+          reviews.push({
             stars: review.FiveStarRating,
             review: review.Review,
-            hours: review.HoursPlayed
-        })
-    })
+            hours: review.HoursPlayed,
+          });
+        });
+        setReviewList(reviews);
+      });
+  }, []);
 
-    setReviewList(reviews)
-  })
-  
   return (
     <div>
       <Link to="/">Home</Link>
       <h1>{name}</h1>
       {<ReviewList reviews={reviewList} />}
+        <div>
+            <CreateReview />
+        </div>
     </div>
   );
 };
 
-export default Game
+export default Game;
