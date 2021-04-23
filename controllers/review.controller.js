ce0cf235c17f36f11a0e5af2bc44ea773c7f8a26
A.Karwowski <akk42@zips.uakron.edu>
2021-04-22 21:15:41 -0400

    a lot of small minor changes like ' -> "

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,54 +1,53 @@
-const mongoose = require('mongoose');
-const Review = require('../models/Reviews')
- 
+const mongoose = require("mongoose");
+const Review = require("../models/Reviews");
 
 module.exports = {
-    create: async (req, res) => {
-        console.log(req.body)
-        try {
-            const new_Review = new Review({
-                GameName: req.body.GameName,
-                HoursPlayed: req.body.HoursPlayed,
-                FiveStarRating: req.body.FiveStarRating,
-                Review: req.body.Review
-            });
-            const return_Review = await new_Review.save();
+  create: async (req, res) => {
+    console.log("Creating Review");
+    try {
+      const new_Review = new Review({
+        GameName: req.body.GameName,
+        HoursPlayed: req.body.HoursPlayed,
+        FiveStarRating: req.body.FiveStarRating,
+        Review: req.body.Review,
+      });
+      const return_Review = await new_Review.save();
 
-            res.status(201).json(return_Review);
-        }
-        catch (err){
-            throw err;
-        }
-    },
-    read: async (req, res) => { 
-        try {
-            const review = await Review.find({
-                _id:req.params.Review_ID
-            });
-            if (!review || review.length == 0){
-                res.status(404).json({
-                    message: "Review Not Found"
-                });
-            }
-            res.status(201).json(review);
-        }
-        catch (err){
-            throw err;
-        }
-    },
-    readGame: async (req, res) => {
-        console.log(req.params.GameName)
-        try{
-            const reviews = await Review.find({
-                GameName: req.params.GameName
-            });
-            res.status(201).json({
-                reviews: reviews
-            });
-        }
-        catch (err){
-            throw err;
-        }
-    } 
-}
+      res.status(200).json(return_Review);
+    } catch (err) {
+      throw err;
+    }
+  },
+  read: async (req, res) => {
+    console.log("Reading Review");
+    try {
+      const review = await Review.find({
+        _id: req.params.Review_ID,
+      });
+      if (!review || review.length == 0) {
+        res.status(404).json({
+          message: "Review Not Found",
+        });
+      }
+      res.status(200).json(review);
+    } catch (err) {
+      throw err;
+    }
+  },
+  readGame: async (req, res) => {
+    console.log("Reading Reviews For Game");
+    console.log(req.params.GameName);
+    try {
+      const reviews = await Review.find({
+        GameName: { $regex: req.params.GameName, $options: "i" },
+      });
+      console.log(reviews);
 
+      res.status(200).json({
+        reviews: reviews,
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+};
--- controllers/title.controller.js
+++ controllers/title.controller.js
@@ -1,17 +1,37 @@
-const mongoose = require('mongoose');
-const Title = require('../models/GameTitle')
- 
-module.exports = {
-    read: async (req, res) => {
-        try{
-            const titles = await Title.find();
-            res.status(201).json({
-                titles: titles
-            });
-        }
-        catch (err){
-            throw err;
-        }
-    } 
-}
+const mongoose = require("mongoose");
+const Title = require("../models/GameTitle");
 
+module.exports = {
+  read: async (req, res) => {
+    console.log("Reading Title List");
+    try {
+      const titles = await Title.find();
+      console.log("Read Game List");
+      res.status(201).json({
+        titles: titles,
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  readOne: async (req, res) => {
+    console.log("Read Game List");
+    try {
+      console.log("Pulling Names");
+      let titles = await Title.find({
+        Name: { $regex: `${req.body.GameName}`, $options: "i" },
+      });
+      console.log("Finished Reading");
+      if (req.body.GameName == "") {
+        titles = [titles[Math.floor(Math.random() * titles.length)]];
+      }
+      if (titles.length == 0) console.log("No results found");
+      console.log("Read Game List");
+      res.status(201).json({
+        titles: titles,
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+};
