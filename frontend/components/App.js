067358656b8e4cfdd8542110c2e0fa6abd93b0d0
A.Karwowski <akk42@zips.uakron.edu>
2021-04-22 21:15:56 -0400

    add caching data

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 import Home from './Home'
 import Game from './Game'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 export default class App extends Component {
-    render(){
-        return (
-            <div>
-                <h2>Hello World</h2>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route path="/title/:name" component={Game} />
-                    </Switch>
-                </BrowserRouter>
-            </div>
-            
-        )
+  constructor(props) {
+    super(props)
+    this.state = {
+      cache: null
     }
+  }
+
+  setCache(cacheData) {
+    this.setState({ cache: cacheData })
+  }
+
+  cache() {
+    return this.state.cache
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hello World</h2>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" >
+              <Home setCache={this.setCache.bind(this)} cache={this.cache.bind(this)} />
+            </Route>
+            <Route path="/title/:name" component={Game} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+
+    )
+  }
 }
