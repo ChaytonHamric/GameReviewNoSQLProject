cc3fbe014f90df4e65c6efdc1e7c995318c966cb
A.Karwowski <akk42@zips.uakron.edu>
2021-04-26 15:33:49 -0400

    add navbar

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -27,12 +27,9 @@ export default function Game(props) {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
         {/* <center> */}
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
